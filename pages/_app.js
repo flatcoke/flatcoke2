b4ab5d494f96c82b579c284bef87e5b6808e43c6
FLATCOKE <flatcoke89@gmail.com>
2019-02-08 00:46:48 +0900

    add material ui

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,19 @@ import { Provider } from 'react-redux'
 import App, { Container } from 'next/app'
 import withRedux from 'next-redux-wrapper'
 import Head from 'next/head'
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import JssProvider from 'react-jss/lib/JssProvider'
+import getPageContext from '../lib/getPageContext'
 import initStore from '../store'
 
 export default withRedux(initStore)(
   class BlogApp extends App {
+    constructor() {
+      super()
+      this.pageContext = getPageContext()
+    }
+
     static async getInitialProps({ Component, ctx }) {
       let pageProps = {}
 
@@ -19,6 +28,14 @@ export default withRedux(initStore)(
       return { pageProps }
     }
 
+    componentDidMount() {
+      // Remove the server-side injected CSS.
+      const jssStyles = document.querySelector('#jss-server-side')
+      if (jssStyles && jssStyles.parentNode) {
+        jssStyles.parentNode.removeChild(jssStyles)
+      }
+    }
+
     renderHead = () => {
       return (
         <Head>
@@ -28,7 +45,7 @@ export default withRedux(initStore)(
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
           />
           <link
-            href="//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/spoqa-han-sans/2.1.2/css/SpoqaHanSans-kr.css"
             rel="stylesheet"
             type="text/css"
           />
@@ -40,16 +57,27 @@ export default withRedux(initStore)(
       const { Component, pageProps, store } = this.props
       return (
         <Container>
-          <Provider store={store}>
-            {this.renderHead()}
-            <style jsx global>{`
-              body {
-                font-family: 'Spoqa Han Sans', sans-serif;
-                font-weight: 300;
-              }
-            `}</style>
-            <Component {...pageProps} />
-          </Provider>
+          <JssProvider
+            registry={this.pageContext.sheetsRegistry}
+            generateClassName={this.pageContext.generateClassName}
+          >
+            <MuiThemeProvider
+              theme={this.pageContext.theme}
+              sheetsManager={this.pageContext.sheetsManager}
+            >
+              <Provider store={store}>
+                {this.renderHead()}
+                <style jsx global>{`
+                  body {
+                    font-family: 'Spoqa Han Sans', sans-serif;
+                    font-weight: 300;
+                  }
+                `}</style>
+                <CssBaseline />
+                <Component pageContext={this.pageContext} {...pageProps} />
+              </Provider>
+            </MuiThemeProvider>
+          </JssProvider>
         </Container>
       )
     }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import classNames from 'classnames/bind'
 import styles from 'styles/styles.scss'
+import Button from '@material-ui/core/Button'
 import Layout from 'components/Layout'
 
 const cx = classNames.bind(styles)
@@ -8,6 +9,7 @@ const cx = classNames.bind(styles)
 export default () => (
   <Layout title="FLATCOKE">
     <div className={cx('example')}>
+      <Button color="primary">This is a button</Button>
       <ul>
         <li>This is FLATCOKE page</li>
         <li>This is personal blog</li>
