cb1dc2f28f0da44c59e6a0efd7f7ee7e130caf19
FLATCOKE <flatcoke89@gmail.com>
2019-02-17 00:36:15 +0900

    Add post migration file and clean up

--- mocha.setup.js
+++ mocha.setup.js
@@ -1,4 +1,4 @@
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import Enzyme from 'enzyme' // eslint-disable-line
+import Adapter from 'enzyme-adapter-react-16' // eslint-disable-line
 
 Enzyme.configure({ adapter: new Adapter() })
