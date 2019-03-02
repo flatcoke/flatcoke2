9e0cc8a418a46b66eb39c15a4baa97423e309e7e
FLATCOKE <flatcoke89@gmail.com>
2019-03-02 20:25:21 +0900

    update api host

--- client/api/index.js
+++ client/api/index.js
@@ -1,16 +1,14 @@
 import axios from 'axios'
 
-const API_HOST = 'http://localhost:8000'
+const API_HOST = process.env.API_HOST || 'http://localhost:3000'
 
-const getURL = (endpoint) => (
-  API_HOST + endpoint
-)
+const getURL = endpoint => API_HOST + endpoint
 
 export const post = (endpoint, data) => {
   return axios.post(getURL(endpoint), data)
 }
 
-export const get = (endpoint) => {
+export const get = endpoint => {
   return axios.get(getURL(endpoint))
 }
 
@@ -22,6 +20,6 @@ export const put = (endpoint, data) => {
   return axios.put(getURL(endpoint), data)
 }
 
-export const del = (endpoint) => {
+export const del = endpoint => {
   return axios.delete(getURL(endpoint) + endpoint)
 }
